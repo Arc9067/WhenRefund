e00d7d0528b08b6da140a17eee9880e821c5363c
Mikail Rokib <atandarokib@gmail.com>
2023-05-22 20:28:10 +0100

    updated

--- src/Components/Join.jsx
+++ src/Components/Join.jsx
@@ -11,8 +11,25 @@ const Join = () => {
     <section className="py-[50px] w-full bg-white flex justify-center items-center">
       <div className="container flex justify-center w-full items-center gap-5 flex-col">
         <h1 className="title text-center text-5xl uppercase">Join us!</h1>
-        <div className="flex flex-col gap-5 justify-center items-center">
-          <div className="p-6 shadow-2xl">telegram</div>
+        <div className="flex flex-col gap-5 justify-center items-center w-full">
+          <a
+            href=""
+            className="p-6 block w-full md:w-max text-center shadow-2xl capitalize text-4xl"
+          >
+            telegram
+          </a>
+          <a
+            href=""
+            className="p-6 block w-full md:w-max text-center shadow-2xl capitalize text-4xl"
+          >
+            twitter
+          </a>
+          <a
+            href=""
+            className="p-6 block w-full md:w-max text-center shadow-2xl capitalize text-4xl"
+          >
+            etherscan
+          </a>
         </div>
       </div>
     </section>
