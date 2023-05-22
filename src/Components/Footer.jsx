b7ba727b1e07cbe35c21cf755ddbb40da1313e56
Mikail Rokib <atandarokib@gmail.com>
2023-05-22 20:41:01 +0100

    updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,12 @@ import Logo from "../assets/image-removebg-preview.png";
 
 const Footer = () => {
   return (
-    <footer className="py[50px] w-full bg-black">
+    <footer className="py-[50px] w-full bg-black">
       <div className="container flex justify-center items-center">
-        <img src={Logo} alt="" className="w-[10rem]" />
+        <h1 className="flex items-center gap-4 text-white text-4xl">
+          <img src={Logo} alt="" className="w-[5rem]" />
+          $WENRFD
+        </h1>
       </div>
     </footer>
   );
